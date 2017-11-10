61f2bde686ef3ce525f7bc231bd8edef07ea0987
Ben Wallace <ben@lup-ben.phi.lup.ie>
2017-11-10 16:53:08 -0500

    added in GET/PUSH/POST sorting

--- log_sorter.py
+++ log_sorter.py
@@ -4,13 +4,15 @@ import re
 import getopt, sys
 
 # CONSTANTS
-verboseMode = 0
+verboseMode = False
 versionNumber = "1.2a"
 updatedOn = "11/11/2017"
 
 inFileName = "file.txt"
 outFileName = "outfile.txt"
 additional_search = ""
+additional_search_flag = False
+read_file = []
 sorted_outlog = []
 
 def set_outfile(file_name):
@@ -23,11 +25,18 @@ def set_infile(file_name):
    inFileName = file_name
    print (("input file is - %s") % (inFileName))
 
+def set_additional_search_flag_true():
+   global additional_search_flag
+   additional_search_flag = True
+
 def set_verbose_mode(toggle):
-   if toggle == 1:
-      verboseMode = 1
-   elif toggle == 0:
-      verboseMode = 0
+   global verboseMode
+   if toggle:
+      verboseMode = True
+      print "Verbose mode - engaged"
+   elif not toggle:
+      print "Verbose mode - disengaged"
+      verboseMode = False
    else:
       print "unable to set verbose mode to " + str(toggle) + "\nExiting..."
       quit_program()
@@ -35,9 +44,18 @@ def set_verbose_mode(toggle):
 def quit_program():
    sys.exit()
 
+def print_help():
+   print "  log_sorter.py  -  Version " + str(versionNumber)
+   print "params"
+   print "    -a       version number\n    -o var   output to var\n    -i var   input var\n    -v       toggle verbose mode number\n    -g       only process GETs\n    -p       only process PUSHs\n    -q       only process POSTs"
+   print "slack @ben.wallace if you have a question on this"
+   sys.exit()
+
 def get_args():
    fullCmdArgs = sys.argv
    argList = fullCmdArgs[1:]
+   global additional_search
+   global additional_search_flag
 
    # define arguments
    #   a- version number
@@ -59,39 +77,83 @@ def get_args():
 # loads arguments
    for currentArgument, currentValue in arguments:
        if currentArgument in ("-h"):
-           print ("slack Ben if you need help for now\nSTOPPING...")
-           quit_program()
+           print_help()
        elif currentArgument in ("-v"):
-           set_verbose_mode(1)
+           set_verbose_mode(True)
        elif currentArgument in ("-o"):
            set_outfile(currentValue)
        elif currentArgument in ("-a"):
            print (("version number is - %s") % (versionNumber))
-        elif currentArgument in ("-g"):
+       elif currentArgument in ("-g"):
+           additional_search = "Started GET "
+           set_additional_search_flag_true()
            print ("only processing for GETs")
+       elif currentArgument in ("-p"):
+          additional_search = "Started PUSH "
+          set_additional_search_flag_true()
+          print ("only processing for PUSHs")
+       elif currentArgument in ("-q"):
+          additional_search = "Started POST "
+          set_additional_search_flag_true()
+          print ("only processing for POSTs")
        elif currentArgument in ("-i"):
            set_infile(currentValue)
 
-def sort_log_by_time(in_log, arg='desc'):
-   test_file = open(in_log,"r")
-   read_file = test_file.read()
+   if verboseMode:
+     print "Finished processing command line args..."
 
-   read_file2 = read_file.split("\n\n")
-   trinary_list = []
+def read_and_split_inFile(in_file):
+
+   if verboseMode:
+      print "Reading and splitting the infile..."
+
+   test_file = open(in_file,"r")
+   read_file = test_file.read()
+   in_log = read_file.split("\n\n")
+   return in_log
+
+def parse_for_additional_search(in_file, search_string):
+
+   if verboseMode:
+      print "Begin searching and splitting for " + str(search_string) + "..."
+
+   out_file = []
+   search_format = re.compile(search_string)
+
+   for i in range(len(in_file)):
+      current_line = in_file[i]
+      match = re.search(search_format, current_line)
+      if match:
+         out_file.append(in_file[i])
+
+
+
+   return out_file
+
+
+def sort_log_by_time(in_log, arg='desc'):
+
+   if verboseMode:
+       print "Begin sorting by time..."
+
+   list_of_tuples = []
 
    datetime_format = re.compile("\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}")
 
-   for i in range(len(read_file2)):
-       current_line = read_file2[i]
-       match2 = re.search(datetime_format, current_line)
-       if match2:
-           hold_tups = (match2.group(),read_file2[i])
-           trinary_list.append(hold_tups)
+   for i in range(len(in_log)):
+       current_line = in_log[i]
+       match = re.search(datetime_format, current_line)
+       if match:
+           hold_tups = (match.group(),in_log[i])
+           list_of_tuples.append(hold_tups)
 
-   trinary_list.sort()
-   return trinary_list
+   list_of_tuples.sort()
+   return list_of_tuples
 
 def write_file(out_log, out_file, args='append'):
+
+   if verboseMode:
+      print "Begin writing to the file output..."
 
    wf = open(out_file,"a")
 
@@ -100,6 +162,12 @@ def write_file(out_log, out_file, args='append'):
 
 
 get_args()
-sorted_outlog = sort_log_by_time(inFileName)
+
+read_file = read_and_split_inFile(inFileName)
+
+if additional_search_flag:
+   read_file = parse_for_additional_search(read_file, additional_search)
+
+sorted_outlog = sort_log_by_time(read_file)
 
 write_file(sorted_outlog, outFileName)
